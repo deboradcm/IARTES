cf1b100e6548626508982c62c02677d0c100d48b
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-02-28 16:21:48 -0400

    adicionando classe Evento

--- ProjetoFinal/Marvin/app/src/main/java/com/example/marvin/Evento.java
+++ ProjetoFinal/Marvin/app/src/main/java/com/example/marvin/Evento.java
@@ -1,2 +1,22 @@
-package com.example.marvin;public class Evento {
+package com.example.marvin;
+
+public class Evento {
+    private String evento;
+    private Dados dados;
+
+    public String getEvento() {
+        return evento;
+    }
+
+    public void setEvento(String evento) {
+        this.evento = evento;
+    }
+
+    public Dados getDados() {
+        return dados;
+    }
+
+    public void setDados(Dados dados) {
+        this.dados = dados;
+    }
 }
