bbbeea6d7c27bc00ac9f070ac49c2bba27355c1a
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-02-28 16:06:53 -0400

    código para capturar as coordenadas do clique e enviá-las para um servidor

--- ProjetoFinal/Marvin/app/src/main/java/com/example/marvin/MainActivity.java
+++ ProjetoFinal/Marvin/app/src/main/java/com/example/marvin/MainActivity.java
@@ -1,8 +1,11 @@
 package com.example.marvin;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import com.google.gson.Gson;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // Capturar as coordenadas do clique
+                int x = (int) view.getX();
+                int y = (int) view.getY();
+
+                // Criar uma instância da classe Evento e preencher seus campos
+                Evento evento = new Evento();
+                evento.setEvento("clique");
+
+                Dados dados = new Dados();
+                dados.setX(x);
+                dados.setY(y);
+
+                evento.setDados(dados);
+
+                // Converter a instância de Evento em JSON
+                Gson gson = new Gson();
+                String jsonEvento = gson.toJson(evento);
+
+                // Aqui você pode enviar o JSON para o servidor, por exemplo:
+                // enviarParaServidor(jsonEvento);
+            }
+        });
     }
 }
+
