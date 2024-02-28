af9bc7a08d3908ccc982783f9f2625781cb394f3
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-02-28 17:29:39 -0400

    Adicionando dependencia do Gson

--- ProjetoFinal/Marvin/app/build.gradle.kts
+++ ProjetoFinal/Marvin/app/build.gradle.kts
@@ -36,6 +36,10 @@ dependencies {
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+    implementation("org.eclipse.paho:org.eclipse.paho.client.mqttv3:1.2.5")
+    implementation("com.google.code.gson:gson:2.8.8")
+
+
+
 
 }
