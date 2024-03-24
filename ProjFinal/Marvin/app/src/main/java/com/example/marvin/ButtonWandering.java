a62d3f974fabd1258972717c5a22dd1d15cbe98a
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-03-23 23:38:12 -0400

    Lógica de movimento e obtenção de coordenadas do botão que vaga por toda a tela

--- ProjFinal/Marvin/app/src/main/java/com/example/marvin/ButtonWandering.java
+++ ProjFinal/Marvin/app/src/main/java/com/example/marvin/ButtonWandering.java
@@ -10,20 +10,44 @@ public class ButtonWandering {
     private static int clickGeneralCount = 0;
 
 
-    public static void setupRandomMoveOnClick(final Button button, final WindowManager windowManager, final Button replacementButton) {
+    public static void setupRandomMoveOnClick(final Button button, final WindowManager windowManager, final Button replacementButton, MainActivity mainActivity) {
         button.setOnClickListener(new View.OnClickListener() {
+
             @Override
-            public void onClick(View v) {
+            public void onClick(View view) {
+
                 clickCount++;
                 clickGeneralCount++;
-                if (clickCount >= 2 && clickGeneralCount < 20) {
+
+
+                if (clickCount == 2 && clickGeneralCount < 20 ) {
+                    // Obter as coordenadas após o movimento do botão
+                    int x = (int) button.getX();
+                    int y = (int) button.getY();
+                    String buttonTag = (String) button.getTag();
+
+                    // Enviar dados para o servidor
+                    mainActivity.sendDataToServer("click", x, y, buttonTag);
+
+                    // movimenta o botão
                     moveButtonRandomly(button, windowManager);
                     clickCount = 0;
 
-                } else if (clickGeneralCount >= 20) {
+
+                } else if (clickCount < 2 && clickGeneralCount < 20) {
+                    // Obter as coordenadas após o movimento do botão
+                    int x = (int) button.getX();
+                    int y = (int) button.getY();
+                    String buttonTag = (String) button.getTag();
+
+                    // Enviar dados para o servidor
+                    mainActivity.sendDataToServer("click", x, y, buttonTag);
+
+                } if (clickGeneralCount > 20) {
                     button.setVisibility(View.GONE);
                     replacementButton.setVisibility(View.VISIBLE);
                 }
+
             }
         });
     }
