0a6bd527a22f10f0b1591515cf7f39c2c91ecb88
deboradcm <debora.medeiros@icomp.ufam.edu.br>
2024-04-24 17:37:53 -0400

    adicionando arquivo de config.rviz

--- ProjFinal/ros2_marvin/src/marvin_description/launch/display.launch.py
+++ ProjFinal/ros2_marvin/src/marvin_description/launch/display.launch.py
@@ -1,38 +1,72 @@
-from launch import LaunchDescription
-from launch_ros.parameter_descriptions import ParameterValue
-from launch_ros.actions import Node
-from launch.substitutions import Command
 import os
-from ament_index_python.packages import get_package_share_path
+from launch import LaunchDescription
+from launch_ros.actions import Node
+from launch.actions import DeclareLaunchArgument
+from launch.conditions import IfCondition, UnlessCondition
+from launch.substitutions import LaunchConfiguration
+
 
 def generate_launch_description():
+    urdf_file = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "..", "..", "marvin_description", "urdf", "marvin.urdf"
+    )
 
-    urdf_path = os.path.join(get_package_share_path('marvin_description'),
-                             'urdf', 'marvin.urdf')
-    rviz_config_path = os.path.join(get_package_share_path('marvin_description'),
-                                    'rviz', 'urdf_config.rviz')
-    
-    robot_description = ParameterValue(Command(['xacro ', urdf_path]), value_type=str)
+    with open(urdf_file, "r") as infp:
+        robot_desc = infp.read()
+
+    rviz_config = os.path.join(
+        os.path.dirname(os.path.abspath(__file__)),
+        "..", "..", "marvin_description", "rviz", "config.rviz"
+    )
 
     robot_state_publisher_node = Node(
         package="robot_state_publisher",
         executable="robot_state_publisher",
-        parameters=[{'robot_description': robot_description}]
+        name="robot_state_publisher",
+        output="screen",
+        parameters=[{"robot_description": robot_desc}],
+    )
+
+    joint_state_publisher_node = Node(
+        package="joint_state_publisher",
+        executable="joint_state_publisher",
+        name="joint_state_publisher",
+        condition=UnlessCondition(LaunchConfiguration("gui")),
     )
 
     joint_state_publisher_gui_node = Node(
         package="joint_state_publisher_gui",
-        executable="joint_state_publisher_gui"
+        executable="joint_state_publisher_gui",
+        name="joint_state_publisher_gui",
+        condition=IfCondition(LaunchConfiguration("gui")),
     )
 
-    rviz2_node = Node(
+    rviz_node = Node(
         package="rviz2",
         executable="rviz2",
-        arguments=['-d', rviz_config_path]
+        name="rviz2",
+        output="screen",
+        arguments=["-d", rviz_config],  # Corrigido aqui para usar a variável rviz_config
     )
 
-    return LaunchDescription([
-        robot_state_publisher_node,
-        joint_state_publisher_gui_node,
-        rviz2_node
-    ])
+    return LaunchDescription(
+        [
+            DeclareLaunchArgument(
+                name="gui",
+                default_value="True",
+                description="Flag to enable joint_state_publisher_gui",
+            ),
+            DeclareLaunchArgument(
+                name="rvizconfig",
+                default_value=rviz_config,
+                description="Absolute path to rviz config file",
+            ),
+            joint_state_publisher_node,
+            joint_state_publisher_gui_node,
+            robot_state_publisher_node,
+            rviz_node,
+        ]
+    )
+
+
